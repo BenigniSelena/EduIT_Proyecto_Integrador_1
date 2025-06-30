5dc06d38eabba1c33e310e3b979a71c6531a10b3
Selena <benigniselena@gmail.com>
2025-06-29 21:50:29 -0300

    ADD laser shots in player

--- Assets/Scripts/SpaceShipPlayer.cs
+++ Assets/Scripts/SpaceShipPlayer.cs
@@ -1,44 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpaceShipPlayer : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 35f;
+    [SerializeField] private float moveSpeed = 50f;
 
-    private Vector3 startPosition;
-
-    private void Start()
-    {
-        startPosition = transform.position;
-    }
+    private Vector3 playerMovement;
+    [Space]
+    [SerializeField] private Text myText;
+    [SerializeField] private GameObject laserShots;
+    [SerializeField] private int shotsFired = 0;
+    [SerializeField] private Transform laserSpawn;
 
     private void Update()
     {
-        Vector3 currentPosition = transform.position;
+        MovePlayer();
+        if (shotsFired >= 1)
+        {
+            UpdateUI();
+        }
+        Shoot();
+    }
 
-        currentPosition.x = startPosition.x;
-        currentPosition.z = startPosition.z;
-        transform.position = currentPosition;
+    private void MovePlayer()
+    {
+        playerMovement = Vector3.zero;
 
         if (Input.GetKey(KeyCode.D))
         {
-            startPosition.x += moveSpeed * Time.deltaTime;
+            playerMovement.x += 1;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            startPosition.x -= moveSpeed * Time.deltaTime;
+            playerMovement.x -= 1;
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            startPosition.z += moveSpeed * Time.deltaTime;
+            playerMovement.z += 1;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            startPosition.z -= moveSpeed * Time.deltaTime;
+            playerMovement.z -= 1;
+        }
+
+        Vector3 currentPosition = transform.position;
+        currentPosition += playerMovement.normalized * moveSpeed * Time.deltaTime;
+        transform.position = currentPosition;
+    }
+
+    private void UpdateUI()
+    {
+        myText.text = "Disparos: " + shotsFired.ToString();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GameObject laser = Instantiate(laserShots, laserSpawn.position, Quaternion.identity);
+            laser.SetActive(true);
+
+            shotsFired += 1;
         }
     }
-}
+} 
